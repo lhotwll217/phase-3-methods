c8bef6bfdfc007a1e84701256f7bf7f1e30d314d
lhotwll217 <lukehenryotwell@gmail.com>
2021-08-09 12:30:55 -0400

    commit tree e72159c49dd74685c32217b07d42f2f9341c4d85

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def my_method(param)
+    puts "Running my_method"
+    param + 1
+end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default( name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(x,y)
+    x + y
+end
+
+def halve (x) 
+    if x.class != Integer
+        return nil
+    end
+
+    x / 2
+end
